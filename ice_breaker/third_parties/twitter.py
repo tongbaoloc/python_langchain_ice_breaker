09f312617aba0782f391f57f67b6f783a7566d59
loctong <loctong>
2023-08-09 09:59:47 +0700

    twitter processing - fixed forbidden issue #1

--- ice_breaker/third_parties/twitter.py
+++ ice_breaker/third_parties/twitter.py
@@ -1,46 +1,37 @@
 import logging
 import os
-from datetime import datetime, timezone
 
 import tweepy
 
 logger = logging.getLogger("twitter")
 
-auth = tweepy.OAuthHandler(
-    os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_API_SECRET")
+twitter_client = tweepy.Client(
+    bearer_token=os.getenv("TWITTER_BEARER_TOKEN"),
+    consumer_key=os.getenv("TWITTER_API_KEY"),
+    consumer_secret=os.getenv("TWITTER_API_SECRET"),
+    access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
+    access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET"),
 )
 
-auth.set_access_token(
-    os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-)
-
-api = tweepy.API(auth)
-
-def scrape_user_tweets(username, num_tweets=20):
+def scrape_user_twitter(username, num_tweets=5):
     """
-    Scrapes a Twitter user's original tweets (i.e., not retweets or replies) and returns them as a list of dictionaries.
-    Each dictionary has three fields: "time_posted" (relative to now), "text", and "url".
+        Scrape a Twitter user's original tweets (i.e, not retweets) and return them as a list of dictionaries.
+        Each dictionary has three fields: "text", "time_posted" (relative to now), and "url".
     """
 
-    tweets = api.user_timeline(screen_name=username, count=num_tweets, include_rts=False, tweet_mode="extended")
+    user_id = twitter_client.get_user(username=username).data.id
+    tweets = twitter_client.get_users_tweets(id=user_id, max_results=num_tweets, exclude= ['retweets', 'replies'])
 
     tweet_list = []
+    for tweet in tweets.data:
+        tweet_dict = {}
+        tweet_dict["text"] = tweet['text']
+        tweet_dict[
+            "url"
+        ] = f"https://twitter.com/{username}/status/{tweet['id']}"
+        tweet_list.append(tweet_dict)
 
+    return tweet_list
 
-    for tweet in tweets:
-        if "RT @" not in tweet.text and not tweet.text.startswith("@"):
-            tweet_dict = {}
-            tweet_dict["time_posted"] = str(
-                datetime.now(timezone.utc) - tweet.created_at
-            )
-            tweet_dict["text"] = tweet.text
-            tweet_dict[
-                "url"
-            ] = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
-            tweet_list.append(tweet_dict)
-
-        return tweet_list
-
-
-
-
+if __name__ == "__main__":
+    print(scrape_user_twitter("elonmusk"))
